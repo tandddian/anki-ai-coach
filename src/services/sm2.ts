f7daaae6b7ad49dbc57f6932a1f23a9e071719a8
tandddian <kirintan123@gmail.com>
2026-06-28 15:46:11 +0800

    chore: add SM-2 input/output interfaces

---
+++ src/services/sm2.ts
@@ -0,0 +1,13 @@
+export interface SM2Input {
+  quality: number; // 0-5
+  repetitions: number;
+  easeFactor: number;
+  interval: number;
+}
+
+export interface SM2Output {
+  repetitions: number;
+  easeFactor: number;
+  interval: number;
+  nextReview: Date;
+}
